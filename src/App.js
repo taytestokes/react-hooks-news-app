52943836e17c018755cf64844bece564c4a0d812
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-04 14:06:57 -0600

    fetched news from hacker news api using fetch api and react hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+  //state
+  const [news, setNews] = useState([]);
+
+  //fetch news
+  const fetchNews = () => {
+    //this will fetch news from hacker news api
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    .then(result => result.json())
+    .then(data => setNews(data.hits))
+    .catch(error => console.log(error));
+  };
+
+  //lifecycle hooks
+  useEffect(() => {
+    fetchNews();
+  });
+
+  //return jsx
   return (
     <div>
-      
+      <h2>News</h2>
+      {
+        news.map((element, index) => (<p key={index}>{element.title}</p>))
+      }
     </div>
-  );
-}
+  )
+};
 
 export default App;
