f5bbebab2c28045d9845520215fcd41ece6d6339
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-04 14:23:19 -0600

    setup a control for the useEffect hook behaviour

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ const App = () => {
   //state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
 
   //methods
   const fetchNews = () => {
     //this will fetch news from hacker news api
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error));
@@ -19,17 +20,22 @@ const App = () => {
     setSearchQuery(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  }
+
   //lifecycle hooks
     //the second argument is an array of items from state telling the useEffect to only fire when there is a change for thos items in state
   useEffect(() => {
     fetchNews();
-  }, [searchQuery]);
+  }, [url]);
 
   //return jsx
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange}/>
         <button>Search</button>
       </form>
