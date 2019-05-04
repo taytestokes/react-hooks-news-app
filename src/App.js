10f52dcbf028e6d920e2f0302c10c3ffd41299d3
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-04 14:36:25 -0600

    refactored code to be more organized and follow more of a FPP

--- src/App.js
+++ src/App.js
@@ -26,7 +26,22 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
-  }
+  };
+
+  const showLoading = () => { { return loading ? <h2>Loading...</h2> : '' } };
+
+  const searchForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+    )
+  };
+
+  const showNews = () => {
+    return news.map((element, index) => (<p key={index}>{element.title}</p>))
+  };
 
   //lifecycle hooks
   //the second argument is an array of items from state telling the useEffect to only fire when there is a change for thos items in state
@@ -38,14 +53,9 @@ const App = () => {
   return (
     <div>
       <h2>News</h2>
-      {loading ? <h2>Loading...</h2> : ''}
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={searchQuery} onChange={handleChange} />
-        <button>Search</button>
-      </form>
-      {
-        news.map((element, index) => (<p key={index}>{element.title}</p>))
-      }
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
   )
 };
